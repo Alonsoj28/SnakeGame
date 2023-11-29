9f0e5ca4993564374bf8365950ad323755ae2df2
Roberto Osorno <69988498+ElJuanNieves@users.noreply.github.com>
2023-11-28 23:15:01 -0600

    Update Mongo

--- controllers/data_handler.js
+++ controllers/data_handler.js
@@ -1,65 +1,52 @@
 const User = require('../controllers/Users.js'); // Importa la clase User
 const fs = require('fs');
 const path = require('path');
+const UserModel = require('../models/User');
 
-const dataFilePathUsers = path.join(__dirname, '../data/users.json');
-const dataFilePathScores = path.join(__dirname, '../data/scoreboeard.json'); 
 
-let userList = loadUsers();
-let scoreList = loadScores();
 
-function loadUsers() {
+async function registerUser(username, password, email) {
     try {
-        const data = fs.readFileSync(dataFilePathUsers, 'utf8');
-        return JSON.parse(data);
+        const existingUser = await UserModel.findOne({ $or: [{ username }, { email }] });
+        if (existingUser) {
+            return { error: 'El username o email ya están en uso' };
+        }
+
+        const newUser = new UserModel({ username, password, email });
+        await newUser.save();
+        return { success: 'Usuario registrado exitosamente' };
     } catch (error) {
-        console.error('Error al cargar los usuarios desde users.json:', error);
-        return [];
+        return { error: 'Error al registrar el usuario en la base de datos' };
     }
 }
 
-function saveUsers() {
+
+
+async function getHighScores() {
     try {
-        fs.writeFileSync(dataFilePathUsers, JSON.stringify(userList, null, 2), 'utf8');
+        const allUsers = await UserModel.find({}, 'username gameHistory');
+        const allScores = [];
+
+        // Obtener todos los registros de puntuaciones de todos los usuarios
+        allUsers.forEach(user => {
+            user.gameHistory.forEach(score => {
+                allScores.push({
+                    username: user.username,
+                    score: score.score
+                });
+            });
+        });
+
+        // Ordenar por puntaje y tomar los 10 mejores
+        allScores.sort((a, b) => b.score - a.score);
+        const top10Scores = allScores.slice(0, 10);
+
+        return top10Scores;
     } catch (error) {
-        console.error('Error al guardar los usuarios en users.json:', error);
+        return { error: 'Error al obtener los highScores' };
     }
 }
 
-function loadScores() {
-    try {
-        const data = fs.readFileSync(dataFilePathScores, 'utf8');
-        return JSON.parse(data);
-    } catch (error) {
-        console.error('Error al cargar los puntajes desde scores.json:', error);
-        return [];
-    }
-}
-
-function saveScores() {
-    try {
-        fs.writeFileSync(dataFilePathScores, JSON.stringify(scoreList, null, 2), 'utf8');
-    } catch (error) {
-        console.error('Error al guardar los puntajes en scores.json:', error);
-    }
-}
-
-function getHighScores()
-{
-    return scoreList;
-}
-
-function highScores(score, username) {
-    // Añade la puntuación y el usuario a la lista de highscores
-    scoreList.push({ score, username });
-    // Ordena la lista de highscores de mayor a menor puntuación
-    scoreList.sort((a, b) => b.score - a.score);
-    // Mantén solo los primeros 10 highscores
-    scoreList = scoreList.slice(0, 10);
-    saveScores(); // Guarda los cambios en el archivo scores.json
-    return scoreList;
-
-}
 
 function registerUser(username, password, email) {
     // Verificar si el usuario o el email ya existen
@@ -76,29 +63,29 @@ function registerUser(username, password, email) {
     return { success: 'Usuario registrado exitosamente' };
 }
 
-function getUserByUsername(username){
-    return userList.find(function (user){
-        return user.username === username;
-    });
+
+async function getUserByUsername(username) {
+    return UserModel.findOne({ username });
 }
-function getUserByEmail(email){
-    return userList.find(function (user){
-        return user.email === email;
-    });
+async function getUserByEmail(email) {
+    return UserModel.findOne({ email });
 }
 
-function addScore(username, score) {
-    const user = getUserByUsername(username);
+async function addScore(username, score) {
+    try {
+        const user = await UserModel.findOne({ username });
+        if (!user) {
+            return { error: 'Usuario no encontrado' };
+        }
 
-    if (!user) {
-        return { error: 'Usuario no encontrado' };
+        user.gameHistory.push({ date: new Date(), score });
+        await user.save();
+        return { success: 'Puntuación registrada exitosamente' };
+    } catch (error) {
+        return { error: 'Error al añadir la puntuación' };
     }
-
-    user.addToGameHistory(new Date().toISOString(), score);
-    saveUsers();
-
-    return { success: 'Puntuación registrada exitosamente' };
 }
+
 function getUserGameHistory(username) {
     const user = getUserByUsername(username);
 
@@ -110,15 +97,11 @@ function getUserGameHistory(username) {
     return gameHistory;
 }
 
-loadUsers()
-console.table(userList);
-console.log(getUserByUsername("user1"));
 
 
 module.exports = {
     registerUser: registerUser,
     getUserByUsername: getUserByUsername,
-    highScores: highScores,
     getUserGameHistory: getUserGameHistory,
     addScore: addScore,
     getHighScores: getHighScores
