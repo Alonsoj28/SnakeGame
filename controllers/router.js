e5d48b89912eec9b15924d92f51090d4ecfa70d4
Roberto Osorno <69988498+ElJuanNieves@users.noreply.github.com>
2023-11-28 21:43:16 -0600

    Update router.js

--- controllers/router.js
+++ controllers/router.js
@@ -8,14 +8,14 @@ router.use('/views', express.static(path.join(__dirname, 'views')));
 router.use('/controllers', express.static(path.join(__dirname, 'controllers')));
 
 
-/*
 const UserRouter = require('../routes/users');
-const LeaderboardRouter = require('../routes/leaderboard');
 
 
 router.use(express.static('public', { 'extensions': ['js'] }));
+router.use('/snake', UserRouter)
 
-*/
 router.get('/', (req,res)=> res.sendFile(path.resolve(__dirname + "/../FrontEnd/index.html")));
+
+
 
 module.exports = router;
