fd963261765d9f3b282b0c3a5b0e9430957ded07
Roberto Osorno <69988498+ElJuanNieves@users.noreply.github.com>
2023-11-28 19:14:29 -0600

    Update

--- controllers/router.js
+++ controllers/router.js
@@ -15,10 +15,7 @@ const LeaderboardRouter = require('../routes/leaderboard');
 
 router.use(express.static('public', { 'extensions': ['js'] }));
 
-router.use('/products', productRouter);
-router.use('/admin', validateAdmin, adminProductRouter);
 */
-
 router.get('/', (req,res)=> res.sendFile(path.resolve(__dirname + "/../FrontEnd/index.html")));
 
 module.exports = router;
