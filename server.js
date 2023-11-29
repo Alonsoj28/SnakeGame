2e827f32739ff840971826a0e51f99280ac7a837
Alonsoj28 <juan.alonso@iteso.mx>
2023-11-28 21:05:09 -0600

    Updated static middleware for css elements and index get request

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const port = 3000;
+const path = require('path');
 
 // Importa el router que configuraste en router.js
 const router = require('./controllers/router.js');
 
+const app = express();
+const port = 3000;
 
-router.get('/', (req, res) => {
-    res.send('e-commerce app práctica 3');
+
+app.use(express.json());
+app.use(router);
+app.use('/views', express.static(path.join(__dirname, 'views')));
+app.use('/FrontEnd', express.static(path.join(__dirname, 'FrontEnd')));
+app.use('/controllers', express.static(path.join(__dirname, 'controllers')));
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/FrontEnd/index.html"));
   });
   
 
-router.listen(port, () => {
+  app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
 });
