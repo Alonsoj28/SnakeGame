0e0035d0d2cda6a3d7d1abb924b622f080cca0c7
Roberto Osorno <69988498+ElJuanNieves@users.noreply.github.com>
2023-11-28 18:02:02 -0600

    Update

--- controllers/data_handler.js
+++ controllers/data_handler.js
@@ -1,16 +1,44 @@
 const Users = require('../controllers/Users.js')
 const fs = require('fs');
 const path = require('path')
-const dataFilePath = path.join(__dirname, '../data/users.json');
+const dataFilePathUsers = path.join(__dirname, '../data/users.json');
+const dataFilePathScoreBoard = path.join(__dirname, '../data/users.json');
 
 let user_lsit = loadUsers();
+let score_list = loadaScores();
 
-function loadProducts() {
+function loadaUsers() {
     try {
-        const data = fs.readFileSync(dataFilePath, 'utf8');
+        const data = fs.readFileSync(dataFilePathUsers, 'utf8');
         return JSON.parse(data);
     } catch (error) {
-        console.error('Error al cargar los productos desde products.json:', error);
+        console.error('Error al cargar los productos desde users.json:', error);
         return [];
+    }
+}
+
+function saveUsers() {
+    try {
+        fs.writeFileSync(dataFilePath, JSON.stringify(productsList, null, 2), 'utf8');
+    } catch (error) {
+        console.error('Error al guardar los productos en users.json:', error);
+    }
+}
+
+function loadaScores() {
+    try {
+        const data = fs.readFileSync(dataFilePathScoreBoard, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error al cargar los productos desde scores.json:', error);
+        return [];
+    }
+}
+
+function saveScores() {
+    try {
+        fs.writeFileSync(dataFilePathScoreBoard, JSON.stringify(productsList, null, 2), 'utf8');
+    } catch (error) {
+        console.error('Error al guardar los productos en scores.json:', error);
     }
 }
